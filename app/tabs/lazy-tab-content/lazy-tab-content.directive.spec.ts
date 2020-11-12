9c98993101dc1d3017806d90232ddea7c65dacc5
ildar <ildar.icoosoft@gmail.com>
2020-11-12 09:47:52 +0500

    remove tests

--- app/tabs/lazy-tab-content/lazy-tab-content.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LazyTabContentDirective } from './lazy-tab-content.directive';
-
-describe('LazyTabContentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LazyTabContentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
